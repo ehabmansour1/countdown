ff48c84109d2b69c8240e7d04815a90b94bfad2d
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2022-06-16 17:53:29 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+let countDownDate = new Date("Jun 29, 2022 11:00:00").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+  let dateDiff = countDownDate - dateNow;
+
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+// ----------------------loader---------
+let loader = document.querySelector(".loader");
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 1000);
+});
